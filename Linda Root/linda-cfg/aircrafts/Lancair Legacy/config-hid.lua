caffe2fef6c2fb91201445627b75cc1ef46f84a6
joeherwig <joachim.herwig@gmx.net>
2020-05-11 23:45:00 +0200

    inital empty commit without any assignments.

---
+++ Linda Root/linda-cfg/aircrafts/Lancair Legacy/config-hid.lua
@@ -0,0 +1,19 @@
+-- Lancair Legacy config-hid.lua @ 2005112341 (LINDA 3.2.2.944) --
+
+-- ## Unknown Device ## (16D00A380) ## {NoSerNum} ############
+
+
+-- ## LB BU0836A ## (16C005BA0) ## {NoSerNum} ############
+
+JSTK["16C005BA0"][1]="Flaps_decr"
+JSTK["16C005BA0"][2]="Flaps_incr"
+
+-- ## Unknown Joystick ## (044F04020) ## {NoSerNum} ############
+
+
+-- ## Unknown Device ## (1690FE130) ## {NoSerNum} ############
+
+
+-- ## Unknown Device ## (BEEF046D0) ## {NoSerNum} ############
+
+
---
+++ Linda Root/linda/aircrafts/Lancair Legacy/ident.lua
@@ -0,0 +1,1 @@
+simfolder="RealAir Legacy V2"
